8e2d92d446497f62b4eb52ecf1dc6699162e010e
Mr-Axinn <79925761+Mr-Axinn@users.noreply.github.com>
2021-03-03 13:38:46 -0500

    New changes in adding methods of people

--- GitHub Demo Lesson/Main.java
+++ GitHub Demo Lesson/Main.java
@@ -1,13 +1,49 @@
 public class Main
 {
     // instance variables - replace the example below with your own
-    public static void main()
-    {
-        System.out.println("hello world");
-		System.out.println("Sam's contribution");
+    //"You win GitHub"
+    //reverse string: buHtiG niw uoY
+    //convert to binary: 01100010 01110101 01001000 01110100 01101001 01000111 00100000 01101110 01101001 01110111 00100000 01110101 01101111 01011001
+    //switch 1 and 0s:
+    
+    public static void main(){
+        leah();
+        josh();
+        gauri();
+        mati();
+        robert();
+        ilan();
+        System.out.println(charles());
+    }
+
+    public static void leah(String str){
+        //Add your own code
+        
     }
     
-    public static void oliverprintln(){
-        System.out.println("Izzy is super cool");
-    }    
+    public static void josh(String str){
+        //Add your own code
+        
+    }
+    public static void gauri(String str){
+        //Add your own code
+        
+    }
+    public static void mati(String str){
+        //Add your own code
+        
+    }
+    public static void robert(String str){
+        //Add your own code
+        
+    }
+    public static void ilan(String str){
+        //Add your own code
+        
+    }
+    public static String charles(String str){
+        //reverse "str"
+        return "hi";
+    }
+    
 }
