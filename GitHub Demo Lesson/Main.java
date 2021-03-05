b4e8581443b6a08eea9b3c048b0d68aaa9987320
Sam Collier <samcollier916@gmail.com>
2021-03-04 22:24:09 -0500

    Canonical Solution

--- GitHub Demo Lesson/Main.java
+++ GitHub Demo Lesson/Main.java
@@ -1,73 +1,74 @@
+/*
+* CRACK THE CODE COLLABORATIVELY
+* Using the power of git we should crack this message in no time!
+*
+* message: imihjezglhzjifwzhqmoziaxifghhbmoehj8i0sjf
+*/
+
 import java.math.BigInteger;
 
 public class Main
 {
-	//Check my work
-	// instance variables - replace the example below with your own
-	//"You win GitHub"
-	//reverse string: buHtiG niw uoY
-	//convert from binary to string: 0110001001110101010010000111010001101001010001110010000001101110011010010111011100100000011101010110111101011001
-	//switch 1 and 0s: 1001110110001010101101111000101110010110101110001101111110010001100101101000100011011111100010101001000010100110
-	//hexadecimal to binary 9D8AB78B96B8DF919688DF8A90A6
-	//substitute letters to their numeric position in the alphabet (A = 1, B = 2, etc. and represent values in hex) imhakghkifkhmoiaifhhmohaiaf
-	//change z to a: imhzkghkifkhmoizifhhmohzizf
-	//remove every third character beginning at character 2 (0, 1, 2 <-- remove) imshzdkgfhkjifpkhxmouizdiffhhtmoqhzfizpf
-
 	public static void main(String[] args)
 	{
-		//System.out.println((char) Integer.parseInt("01001000", 2));
-		System.out.println(charles(ilan(robert(mati(gauri(josh(leah("imshzdkgfhkjifpkhxmouizdiffhhtmoqhzfizpf"))))))));
+		System.out.println(charles(ilan(robert(mati(gauri(josh(leah("imihjezglhzjifwzhqmoziaxifghhbmoehj8i0sjf"))))))));
 	}
 
 	public static String leah(String str)
 	{
 		//remove every third character
-
-		//...
-
-		return "";
+		String s = "";
+		for(int i=0;i<str.length();i++)
+			if((i+1)%3!=0) s+=str.charAt(i);
+		return s;
 	}
 
 	public static String josh(String str)
 	{
-		//change all 'z' characters to 'a' characters
-
-		//...
-
-		return "";
+		//change all 'z' characters to 'k' characters
+		//don't worry about uppercase
+		String s = "";
+		for(int i=0;i<str.length();i++)
+			if(str.charAt(i)=='z') s+='k';
+			else s+= str.charAt(i);
+		return s;
 	}
 
 	public static String gauri(String str)
 	{
-		//substitute characters to their numeric position in the alphabet (A = 1, B = 2, etc. and represent position in hex)
-		//so for example if you received the String "feedback" this method should return "6554213B"
-
-		//...
-
-		return "";
+		//substitute letters to their numeric position in the alphabet (A = 1, B = 2, etc. and represent position in hex)
+		//if the character is a digit then the digit will remain as it was
+		//so for example if you received the String "feedb4ack123" this method should return "65542413b123"
+		//the ascii value of 'a' is 97
+		//Character.isDigit(char c) and/or Character.isLetter(char c) and/or Integer.toHexString(int n) may help
+		//don't worry about uppercase
+		String s = "";
+		for(int i=0;i<str.length();i++)
+			if(Character.isDigit(str.charAt(i)))s+=str.charAt(i);
+			else s+=Integer.toHexString((int)str.charAt(i)-96);
+		return s;
 	}
 
 	public static String mati(String str)
 	{
 		//Convert Hexadecimal String to Binary String
 		//the hexadecimal value being passed in here is most likely going to be insanely large
-		//so I recommend you use BigInteger here
-		//you can initialise a BigInteger with a String and specifying that it's base 2 using BigInteger(str,2)
+		//so I recommend you use BigInteger here.
+		//You can initialise a BigInteger with a String and specify that it's base 2 using BigInteger(str,2)
 		//BigInteger has a toString method and you can specify the radix (base system) as a parameter.
-		//For example new BigInteger(str,2).toString(10) converts a binary string to decimal string
-
-		//...
-
-		return "";
+		//For example: new BigInteger(str,2).toString(10) converts a binary string to a decimal string of the same value
+		return new BigInteger(str,16).toString(2);
 	}
 
 	public static String robert(String str)
 	{
 		//switch the 1s and 0s in this string consisting of 1s and 0s
-
-		//...
-
-		return "";
+		//you can assume the only characters in the string being passed in are either '1' or '0'
+		String s = "";
+		for(int i=0;i<str.length();i++)
+			if(str.charAt(i)=='0') s+='1';
+			else s+='0';
+		return s;
 	}
 
 	public static String ilan(String str)
@@ -79,19 +80,19 @@ public class Main
 		//you can specify the radix (base of a system of numeration) of the string as well.
 		//For example int n = Integer.parseInt("4A",16) will evaluate to 74 in decimal
 		//and the value stored in n will be 74
-
-		//...
-
-		return "";
+		String s = "";
+		for(int i=0;i<str.length();i+=8)
+			s+=(char)Integer.parseInt(str.substring(i,i+8),2);
+		return s;
 	}
 
 	public static String charles(String str)
 	{
 		//reverse the String being passed in
-
-		//...
-
-		return "";
+		String s = "";
+		for(int i=str.length()-1;i>=0;i--)
+			s+=str.charAt(i);
+		return s;
 	}
 
 }
